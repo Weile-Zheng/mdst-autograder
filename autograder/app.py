dc198b139be2835ffee6ec6d0b65018736d11eab
Weile <weilez@umich.edu>
2024-09-20 19:51:21 -0400

    login and database support

--- autograder/app.py
+++ autograder/app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, redirect, url_for, request, session, render_template
+from flask import Flask, redirect, url_for, request, session, render_template, flash
 from supabase import create_client, Client
 from dotenv import load_dotenv
 
@@ -16,6 +16,7 @@ supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 def home():
     user = session.get('user')
     if not user: return redirect(url_for("login"))
+    check_user_in_database(user["id"], user["user_full_name"], user["user_email"])
     return render_template('index.html', user=user)
 
 @app.route('/login/')
@@ -29,6 +30,7 @@ def login_auth():
     callback_url = url_for('callback', _external=True) 
     response = supabase.auth.sign_in_with_oauth({"provider": 'google', 
     "options": {"redirect_to": callback_url}})
+    
     return redirect(response.url)
 
 @app.route('/callback/')
@@ -41,7 +43,6 @@ def store_token():
 
     if access_token:
         session['access_token'] = access_token
-
         user_info = supabase.auth.get_user(access_token).user
         user_info_dict = {
             'id':user_info.id,
@@ -58,21 +59,42 @@ def store_token():
 @app.route('/logout/')
 def logout():
     session.clear()
-    return redirect("https://accounts.google.com/Logout")
+    return redirect(url_for('login'))
 
 @app.route('/submit_github_link/', methods=['POST'])
 def submit_github_link():
     github_link = request.form.get('github_link')
-    # Handle the GitHub link submission logic here
     flash('GitHub link submitted successfully!', 'success')
     return redirect(url_for('home'))
 
 @app.route('/upload_checkpoint_files/', methods=['POST'])
 def upload_checkpoint_files():
     files = request.files.getlist('checkpoint_files')
-    # Handle the file upload logic here
     flash('Files uploaded successfully!', 'success')
     return redirect(url_for('home'))
 
+######################################################
+# Helper functions
+######################################################
+
+def check_user_in_database(uid: str, full_name: str, email):
+    """
+    Check if user is in database. If not add user to database
+    """
+    response = supabase.from_('Users').select('google_id').eq('google_id', uid).execute()
+    
+    if not response.data:
+        supabase.from_('Users').insert({
+                'google_id': uid,
+                'full_name': full_name,
+                'email': email,
+            }).execute()
+
+        return redirect(url_for('home'))
+    else:
+        return "Error: Access token not found", 400
+
+
+    
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=8080)
