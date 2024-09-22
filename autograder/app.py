9a66d44d76b768686df14d17476b364240528f97
Weile <weilez@umich.edu>
2024-09-21 23:15:30 -0400

    (feat) add file uploading to supabase. Changes will reflect on homescreen

--- autograder/app.py
+++ autograder/app.py
@@ -1,16 +1,24 @@
 import os
+import requests
 from flask import Flask, redirect, url_for, request, session, render_template, flash
-from supabase import create_client, Client
-from dotenv import load_dotenv
+from werkzeug.datastructures import FileStorage
+from supabase import create_client
+from config import Config
 
-load_dotenv()
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'
+app.config.from_object(Config)
 
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
-supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+supabase = create_client(app.config["SUPABASE_URL"], app.config["SUPABASE_KEY"])
+
+
+UPLOAD_FOLDER = 'uploads'
+MAX_FILE_SIZE = 500 * 1024 # 500 KB for checkpoint files
+ALLOWED_FILENAMES = {'checkpoint0.ipynb', 'checkpoint1.ipynb'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_FILE_SIZE'] = MAX_FILE_SIZE
+app.config['ALLOWED_FILENAMES'] = ALLOWED_FILENAMES
 
 @app.route('/')
 def home():
@@ -81,15 +89,36 @@ def submit_github_link():
 @app.route('/upload_checkpoint_files/', methods=['POST'])
 def upload_checkpoint_files():
     files = request.files.getlist('checkpoint_files')
-    flash('Files uploaded successfully!', 'success')
+    for file in files:
+        if check_file_validity(file):
+            new_filename = strip_uniquename_from_email(session["user"]["user_email"]) + "_" + file.filename
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], new_filename) 
+            file.save(file_path)
+            response = upload_checkpoint_to_supabase(new_filename, file_path)
+            print(response)
+            if response.status_code == 200: 
+                flash('Files uploaded successfully!', 'success')
+                if file.filename == "checkpoint0.ipynb":
+                    session["user"]["checkpoint0"] = "uploaded"
+                elif file.filename == "checkpoint1.ipynb":
+                    session["user"]["checkpoint1"] = "uploaded"
+                session.modified = True
+                return redirect(url_for('home')) 
+            else:
+                flash('File upload failed when sending to database', 'error')
+            os.remove(file_path)
+        else: 
+            print("File not valid: Exceed size 500 KB or is not named checkpoint0.ipynb or checkpoint0.ipynb ")
+            flash('File upload failed!', 'error')
     return redirect(url_for('home'))
 
 ######################################################
 # Helper functions
 ######################################################
-def check_user_in_database(uid: str, full_name: str, email):
+def check_user_in_database(uid: str, full_name: str, email) -> str:
     """
-    Check if user is in database. If not add user to database in Users table. 
+    Check if user is in database. If not add user to database in Users table with 
+    google_id and create entry in the tutorial submission table with null values. 
     """
     response = supabase.from_('Users').select('google_id').eq('google_id', uid).execute()
     
@@ -99,18 +128,46 @@ def check_user_in_database(uid: str, full_name: str, email):
                 'full_name': full_name,
                 'email': email,
             }).execute()
-
-        return redirect(url_for('home'))
     else:
         return "Error: Access token not found", 400
 
-def update_github_link_db(uid:str, github_link:str):
+def update_github_link_db(uid:str, github_link:str) -> None:
     supabase.from_('Users').update({'github_link': github_link}).eq('google_id', uid).execute()
 
 def get_github_link_db(uid:str) -> str:
     response = supabase.from_('Users').select('github_link').eq('google_id', uid).execute()
     if response.data:
         return response.data[0]['github_link']
-    
+
+def check_file_validity (file: FileStorage) -> bool:
+    """
+    Checks if file passes both extension and 
+    """
+    if file.content_length > app.config['MAX_FILE_SIZE']:
+        return False
+    if not file.filename in app.config['ALLOWED_FILENAMES']:
+        return False
+    return True
+
+def upload_checkpoint_to_supabase(filename: str, filepath: str) -> requests.Response: 
+    """
+    Upload a checkpoint file to supabase bucket: "checkpoints".
+
+    If file name already exist in bucket, override. 
+
+    Note this function expects a valid input and does not do additial checkings 
+    on the file type, size etc. 
+    """
+    return supabase.storage.from_('checkpoints').upload(filename, filepath, {'upsert':'true'})
+
+def strip_uniquename_from_email(email: str) -> bool:
+
+    """
+    Strips the unique name from an email address given an email.
+
+    Ex: weilez@umich.edu -> weilez
+    """
+    return email.split('@')[0]
+
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=8080)
