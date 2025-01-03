60f7fa1f5032618a427eb3e78de8ca2af972d6ae
Weile <weilez@umich.edu>
2025-01-03 17:05:45 -0500

    cleanup

--- autograder/__init__.py
+++ autograder/__init__.py
@@ -1,12 +1,9 @@
 # __init__.py
-import os 
 from flask import Flask
 from supabase import create_client
 from autograder.config import Config
-from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent
-app = Flask(__name__, template_folder=BASE_DIR / "templates")
+app = Flask(__name__, template_folder='templates')
 app.config.from_object(Config)
 
 supabase_client = create_client(app.config["SUPABASE_URL"], app.config["SUPABASE_KEY"])
